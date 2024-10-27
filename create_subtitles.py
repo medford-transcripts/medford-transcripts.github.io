6e4dccc160738185f8add1aba6b6ab9e31b9d33c
Jason Eastman (windows) <jdeast@gmail.com>
2024-10-26 23:38:25 -0400

    add videos, speed up regeneration

--- create_subtitles.py
+++ create_subtitles.py
@@ -26,128 +26,128 @@ import ipdb
 ''' 
 def download_audio(yt_id, dir=None):
 
-	files = glob.glob('*' + yt_id + '*/*' + yt_id + '*.mp3')
-	if len(files) == 1: return files[0]
+    files = glob.glob('*' + yt_id + '*/*' + yt_id + '*.mp3')
+    if len(files) == 1: return files[0]
 
-	url = "https://youtu.be/" + yt_id
-	with yt_dlp.YoutubeDL() as ydl:
-		info = ydl.extract_info(url, download=False)
+    url = "https://youtu.be/" + yt_id
+    with yt_dlp.YoutubeDL() as ydl:
+        info = ydl.extract_info(url, download=False)
 
-	for format in info["formats"][::-1]:
-		if format["resolution"] == "audio only" and format["ext"] == "m4a":
-			audio_url = format["url"]
-			break
-	
-	timestamp = datetime.datetime.fromtimestamp(info["timestamp"]).strftime("%Y-%m-%d")
-	title = info["title"].replace(" ","_").replace("#","").replace(",","").replace('"','')
-	if dir==None:
-		dir = timestamp + "_" + yt_id #+ "_" + title.lower()
+    for format in info["formats"][::-1]:
+        if format["resolution"] == "audio only" and format["ext"] == "m4a":
+            audio_url = format["url"]
+            break
+    
+    timestamp = datetime.datetime.fromtimestamp(info["timestamp"]).strftime("%Y-%m-%d")
+    title = info["title"].replace(" ","_").replace("#","").replace(",","").replace('"','')
+    if dir==None:
+        dir = timestamp + "_" + yt_id #+ "_" + title.lower()
 
-	mp3file = os.path.join(dir,dir) +'.mp3'
-	if os.path.isfile(mp3file): return mp3file
+    mp3file = os.path.join(dir,dir) +'.mp3'
+    if os.path.isfile(mp3file): return mp3file
 
-	if not os.path.exists(dir): os.makedirs(dir)
+    if not os.path.exists(dir): os.makedirs(dir)
 
-	ydl_opts = {
-	    'format': 'bestaudio/best',
-	    'outtmpl': os.path.join(dir,dir),
-	    'postprocessors': [{
-	        'key': 'FFmpegExtractAudio',
-	        'preferredcodec': 'mp3',
-	        'preferredquality': '320',
-	    }],
-	}
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'outtmpl': os.path.join(dir,dir),
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '320',
+        }],
+    }
 
-	with yt_dlp.YoutubeDL(ydl_opts) as ydl: 
-	    ydl.download(audio_url)
-	    return mp3file
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl: 
+        ydl.download(audio_url)
+        return mp3file
 
 def generate_output(result, mp3file):
-	subdir = os.path.dirname(mp3file)
-	result["language"] = "en"
-	output_writer = whisperx.utils.get_writer("srt", subdir)
-	output_writer(result, mp3file, {'max_line_width': None,'max_line_count': None,'highlight_words': False})
-	return
+    subdir = os.path.dirname(mp3file)
+    result["language"] = "en"
+    output_writer = whisperx.utils.get_writer("srt", subdir)
+    output_writer(result, mp3file, {'max_line_width': None,'max_line_count': None,'highlight_words': False})
+    return
 
 def transcribe(yt_id, min_speakers=None, max_speakers=None):
-	t0 = datetime.datetime.utcnow()
+    t0 = datetime.datetime.utcnow()
 
-	# command to trim an audio file
-	# ffmpeg -i kP4iRYobyr0.mp3 -ss 0 -to 300 council5.mp3
+    # command to trim an audio file
+    # ffmpeg -i kP4iRYobyr0.mp3 -ss 0 -to 300 council5.mp3
 
-	mp3file = download_audio(yt_id)
-	base = os.path.splitext(mp3file)[0]
-	subdir = os.path.dirname(mp3file)
-	print("Download complete. Took " + str((datetime.datetime.utcnow()-t0).total_seconds()) + " seconds")
+    mp3file = download_audio(yt_id)
+    base = os.path.splitext(mp3file)[0]
+    subdir = os.path.dirname(mp3file)
+    print("Download complete. Took " + str((datetime.datetime.utcnow()-t0).total_seconds()) + " seconds")
 
-	# whisperX options
-	device = "cpu" #"cuda" 
-	batch_size = 16 # reduce if low on GPU mem
-	#compute_type = "float16" # change to "int8" if low on GPU mem (may reduce accuracy)
-	compute_type = "int8" # float16 not supported on my machine (may reduce accuracy)
-	model_dir = "./"
-	model = whisperx.load_model("large-v2", device, compute_type=compute_type, download_root=model_dir, language="en")
+    # whisperX options
+    device = "cpu" #"cuda" 
+    batch_size = 16 # reduce if low on GPU mem
+    #compute_type = "float16" # change to "int8" if low on GPU mem (may reduce accuracy)
+    compute_type = "int8" # float16 not supported on my machine (may reduce accuracy)
+    model_dir = "./"
+    model = whisperx.load_model("large-v2", device, compute_type=compute_type, download_root=model_dir, language="en")
 
-	# basic transcription
-	audio = whisperx.load_audio(mp3file)
-	result = model.transcribe(audio, batch_size=batch_size)
-	generate_output(result, base + '_basic.mp3')
-	print("Transcription complete. Took " + str((datetime.datetime.utcnow()-t0).total_seconds()) + " seconds")
+    # basic transcription
+    audio = whisperx.load_audio(mp3file)
+    result = model.transcribe(audio, batch_size=batch_size)
+    generate_output(result, base + '_basic.mp3')
+    print("Transcription complete. Took " + str((datetime.datetime.utcnow()-t0).total_seconds()) + " seconds")
 
-	# delete model if low on GPU resources
-	# import gc; gc.collect(); torch.cuda.empty_cache(); del model
+    # delete model if low on GPU resources
+    # import gc; gc.collect(); torch.cuda.empty_cache(); del model
 
-	# align whisper output (generate accurate word-level timestamps)
-	model_a, metadata = whisperx.load_align_model(language_code=result["language"], device=device)
-	aligned_result = whisperx.align(result["segments"], model_a, metadata, audio, device, return_char_alignments=False)
-	generate_output(aligned_result, base + '_aligned.mp3')
-	print("Alignment complete. Took " + str((datetime.datetime.utcnow()-t0).total_seconds()) + " seconds")
+    # align whisper output (generate accurate word-level timestamps)
+    model_a, metadata = whisperx.load_align_model(language_code=result["language"], device=device)
+    aligned_result = whisperx.align(result["segments"], model_a, metadata, audio, device, return_char_alignments=False)
+    generate_output(aligned_result, base + '_aligned.mp3')
+    print("Alignment complete. Took " + str((datetime.datetime.utcnow()-t0).total_seconds()) + " seconds")
 
-	# delete model if low on GPU resources
-	# import gc; gc.collect(); torch.cuda.empty_cache(); del model_a
+    # delete model if low on GPU resources
+    # import gc; gc.collect(); torch.cuda.empty_cache(); del model_a
 
-	# Assign speaker labels ("diarization")
-	with open('hf_token.txt') as f: token = f.readline()
-	diarize_model = whisperx.DiarizationPipeline(use_auth_token=token, device=device)
-	diarize_segments = diarize_model(audio, min_speakers=min_speakers, max_speakers=max_speakers)
-	print("Diarization complete. Took " + str((datetime.datetime.utcnow()-t0).total_seconds()) + " seconds")
+    # Assign speaker labels ("diarization")
+    with open('hf_token.txt') as f: token = f.readline()
+    diarize_model = whisperx.DiarizationPipeline(use_auth_token=token, device=device)
+    diarize_segments = diarize_model(audio, min_speakers=min_speakers, max_speakers=max_speakers)
+    print("Diarization complete. Took " + str((datetime.datetime.utcnow()-t0).total_seconds()) + " seconds")
 
-	# segments are now assigned speaker IDs
-	diarize_result = whisperx.assign_word_speakers(diarize_segments, aligned_result)
-	generate_output(diarize_result, base + '.mp3')
+    # segments are now assigned speaker IDs
+    diarize_result = whisperx.assign_word_speakers(diarize_segments, aligned_result)
+    generate_output(diarize_result, base + '.mp3')
 
-	print("Output complete. Took " + str((datetime.datetime.utcnow()-t0).total_seconds()) + " seconds")
+    print("Output complete. Took " + str((datetime.datetime.utcnow()-t0).total_seconds()) + " seconds")
 
 if __name__ == "__main__":
 
-	#yt_id = "kP4iRYobyr0" # city council meeting 2024-10-15
-	#yt_id = "eYLl0XsNfvs" # town hall
-	#yt_id = "kgJb8ZwdaF0" # school committee
-	#transcribe(yt_id)
+    #yt_id = "kP4iRYobyr0" # city council meeting 2024-10-15
+    #yt_id = "eYLl0XsNfvs" # town hall
+    #yt_id = "kgJb8ZwdaF0" # school committee
+    #transcribe(yt_id)
 
-	channels = ["@medfordpublicschools464","@CityofMedfordMass","@InvestinMedford","@ALLMedford"]
-	for channel in channels:
-		url = "https://www.youtube.com/" + channel 
-		info = yt_dlp.YoutubeDL().extract_info(url, download=False) 
+    channels = ["@medfordpublicschools464","@CityofMedfordMass","@InvestinMedford","@ALLMedford"]
+    for channel in channels:
+        url = "https://www.youtube.com/" + channel 
+        info = yt_dlp.YoutubeDL().extract_info(url, download=False) 
 
-		if "display_id" in info["entries"][0].keys():
-			for entry in info["entries"]:
-				yt_id = entry["display_id"]
-				files = glob.glob("*/*_" + yt_id + ".srt")
-				if len(files) == 0:
-					try: 
-						# downloads are heavily throttled by youtube and can be done at the same time as transcription
-						#download_audio(yt_id) 
-						transcribe(yt_id)
-					except: pass
-		else:
-			for playlist in info["entries"]:
-				for entry in playlist["entries"]:
-					yt_id = entry["display_id"]
-					files = glob.glob("*/*_" + yt_id + ".srt")
-					if len(files) == 0:
-						try: 
-							# downloads are heavily throttled by youtube and can be done at the same time as transcription
-							#download_audio(yt_id) 
-							transcribe(yt_id)
-						except: pass
+        if "display_id" in info["entries"][0].keys():
+            for entry in info["entries"]:
+                yt_id = entry["display_id"]
+                files = glob.glob("*/*_" + yt_id + ".srt")
+                if len(files) == 0:
+                    try: 
+                        # downloads are heavily throttled by youtube and can be done at the same time as transcription
+                        #download_audio(yt_id) 
+                        transcribe(yt_id)
+                    except: pass
+        else:
+            for playlist in info["entries"]:
+                for entry in playlist["entries"]:
+                    yt_id = entry["display_id"]
+                    files = glob.glob("*/*_" + yt_id + ".srt")
+                    if len(files) == 0:
+                        try: 
+                            # downloads are heavily throttled by youtube and can be done at the same time as transcription
+                            #download_audio(yt_id) 
+                            transcribe(yt_id)
+                        except: pass
--- srt2html.py
+++ srt2html.py
@@ -1,41 +1,41 @@
-import datetime, os
+import datetime, time, os
 import ipdb
 from wordcloud import WordCloud
 import json,glob
 from googletrans import Translator, constants
 
 def finish_speaker(html, speaker_stats, text, speaker, yt_id, start, stop, eshtml=None):
-	# new speaker; wrap up and start new
-	if text != "":
-		html.write('    <a href="https://youtu.be/' + yt_id + '&t=' + str(start) + 's">')
-		html.write("[" + speaker + "]</a>: " + text + "<br><br>\n\n")
+    # new speaker; wrap up and start new
+    if text != "":
+        html.write('    <a href="https://youtu.be/' + yt_id + '&t=' + str(start) + 's">')
+        html.write("[" + speaker + "]</a>: " + text + "<br><br>\n\n")
 
-		if eshtml != None:
-			translator = Translator()
-			translation = translator.translate(text, dest="es")
-			eshtml.write('    <a href="https://youtu.be/' + yt_id + '&t=' + str(start) + 's">')
-			eshtml.write("[" + speaker + "]</a>: " + translation.text + "<br><br>\n\n")
+        if eshtml != None:
+            translator = Translator()
+            translation = translator.translate(text, dest="es")
+            eshtml.write('    <a href="https://youtu.be/' + yt_id + '&t=' + str(start) + 's">')
+            eshtml.write("[" + speaker + "]</a>: " + translation.text + "<br><br>\n\n")
 
-	# let's do some stats by speaker
-	if not speaker in speaker_stats.keys(): speaker_stats[speaker] = {"words": {}, "all_words" : ""}
-	
-	# time
-	added_time = stop-start
-	if "total_time" in speaker_stats[speaker].keys(): speaker_stats[speaker]["total_time"] += added_time
-	else: speaker_stats[speaker]["total_time"] = added_time
+    # let's do some stats by speaker
+    if not speaker in speaker_stats.keys(): speaker_stats[speaker] = {"words": {}, "all_words" : ""}
+    
+    # time
+    added_time = stop-start
+    if "total_time" in speaker_stats[speaker].keys(): speaker_stats[speaker]["total_time"] += added_time
+    else: speaker_stats[speaker]["total_time"] = added_time
 
-	# all words for word cloud
-	speaker_stats[speaker]["all_words"] += text
+    # all words for word cloud
+    speaker_stats[speaker]["all_words"] += text
 
-	added_words = 0
-	for word in text.split():
-		this_word = word.lower().split(".")[0].split(",")[0]
-		added_words += 1
-		if this_word in speaker_stats[speaker]["words"].keys(): speaker_stats[speaker]["words"][this_word] += 1
-		else: speaker_stats[speaker]["words"][this_word] = 1
+    added_words = 0
+    for word in text.split():
+        this_word = word.lower().split(".")[0].split(",")[0]
+        added_words += 1
+        if this_word in speaker_stats[speaker]["words"].keys(): speaker_stats[speaker]["words"][this_word] += 1
+        else: speaker_stats[speaker]["words"][this_word] = 1
 
-	if "total_words" in speaker_stats[speaker].keys(): speaker_stats[speaker]["total_words"] += added_words
-	else: speaker_stats[speaker]["total_words"] = added_words
+    if "total_words" in speaker_stats[speaker].keys(): speaker_stats[speaker]["total_words"] += added_words
+    else: speaker_stats[speaker]["total_words"] = added_words
 
 def srt2html(yt_id):
 
@@ -43,6 +43,8 @@ def srt2html(yt_id):
     htmlfilename = os.path.splitext(srtfilename)[0] + '.html'
     eshtmlfilename = os.path.splitext(srtfilename)[0] + '.es.html'
     dir = os.path.dirname(srtfilename)
+
+    last_changed = os.path.getmtime(srtfilename)
 
     councilors = ["Lungo-Koehn", # Mayor
     "Bears","Collins","Callahan","Lazzaro","Leming","Scarpelli","Tseng", # City Councilors
@@ -53,14 +55,31 @@ def srt2html(yt_id):
     start = 0.0
     stop = 86400.0
     text = ""
-    t0 = datetime.datetime(1900,1,1)	
+    t0 = datetime.datetime(1900,1,1)    
 
     # read in the speaker mappings
     jsonfile = os.path.join(dir,'speaker_ids.json')
     if os.path.exists(jsonfile):
-	    with open(jsonfile, 'r') as fp:
-    		speaker_ids = json.load(fp)
+        with open(jsonfile, 'r') as fp:
+            speaker_ids = json.load(fp)
+        last_changed = max(os.path.getmtime(jsonfile),last_changed)
     else: speaker_ids = {}
+
+    ### if the speaker IDs or SRT file haven't been updated since we did this last, no need to redo it ###
+    jsonfile = os.path.join('video_data.json')
+    if os.path.exists(jsonfile):
+        with open(jsonfile, 'r') as fp:
+            video_data = json.load(fp)
+    else:
+        video_data = {}
+
+    last_update = 0.0
+    if yt_id in video_data.keys():
+        if "last_update" in video_data[yt_id].keys():
+            last_update = video_data[yt_id]["last_update"]
+
+    if last_update > last_changed: return
+    #######################################################################################################
 
     # output custom html with links to corresponding parts of the youtube video
     html = open(htmlfilename, 'w', encoding="utf-8")
@@ -83,81 +102,81 @@ def srt2html(yt_id):
     with open(srtfilename, 'r', encoding="utf-8") as file:
 
         # Read each line in the file
-	    for line in file:
-	        line.strip()
+        for line in file:
+            line.strip()
 
-	        if "-->" in line:
-	        	# timestamp
-	        	start_string = line.split()[0]
-	        	stop_string = line.split()[-1]
+            if "-->" in line:
+                # timestamp
+                start_string = line.split()[0]
+                stop_string = line.split()[-1]
 
-	        	# convert timestamp to seconds elapsed
-	        	this_start = (datetime.datetime.strptime(start_string,'%H:%M:%S,%f')-t0).total_seconds()
-	        	this_stop = (datetime.datetime.strptime(stop_string,'%H:%M:%S,%f')-t0).total_seconds()
+                # convert timestamp to seconds elapsed
+                this_start = (datetime.datetime.strptime(start_string,'%H:%M:%S,%f')-t0).total_seconds()
+                this_stop = (datetime.datetime.strptime(stop_string,'%H:%M:%S,%f')-t0).total_seconds()
 
-	        elif "[" in line:
-	        	# text
-	        	this_speaker = line.split()[0].split("[")[-1].split("]")[0]
-	        	this_text = line.split(":")[-1]
+            elif "[" in line:
+                # text
+                this_speaker = line.split()[0].split("[")[-1].split("]")[0]
+                this_text = line.split(":")[-1]
 
-	        	# replace automated speaker tag with speaker ID
-	        	if this_speaker in speaker_ids.keys():
-	        		this_speaker = speaker_ids[this_speaker]
+                # replace automated speaker tag with speaker ID
+                if this_speaker in speaker_ids.keys():
+                    this_speaker = speaker_ids[this_speaker]
 
-	        	if this_speaker == speaker:
-	        		# same speaker; append to previous text
-	        		text += this_text
-	        	else:
-	        		finish_speaker(html, speaker_stats, text, speaker, yt_id, start, stop, eshtml=eshtml)
+                if this_speaker == speaker:
+                    # same speaker; append to previous text
+                    text += this_text
+                else:
+                    finish_speaker(html, speaker_stats, text, speaker, yt_id, start, stop, eshtml=eshtml)
 
-	        		# update to new values
-	        		start = this_start
-	        		text = this_text
-	        		speaker = this_speaker
+                    # update to new values
+                    start = this_start
+                    text = this_text
+                    speaker = this_speaker
 
-	        	stop = this_stop
+                stop = this_stop
 
-	        else: continue
+            else: continue
 
     finish_speaker(html, speaker_stats, text, speaker, yt_id, start, stop, eshtml=eshtml)
 
     for speaker in speaker_stats.keys():
-    	nprinted = 0
-    	if speaker != '':
-	    	print(speaker + ': ')
-	    	print("  total time: " + str(round(speaker_stats[speaker]["total_time"]/60.0,2)) + ' minutes')
-	    	print("  total words: " + str(speaker_stats[speaker]["total_words"]))
+        nprinted = 0
+        if speaker != '':
+            print(speaker + ': ')
+            print("  total time: " + str(round(speaker_stats[speaker]["total_time"]/60.0,2)) + ' minutes')
+            print("  total words: " + str(speaker_stats[speaker]["total_words"]))
 
-	    	# make a word cloud
-	    	if speaker in councilors:
-		    	wordcloud = WordCloud(max_font_size=40).generate(speaker_stats[speaker]["all_words"])
-		    	wordcloud.to_file(os.path.join(dir,speaker + '.wordcloud.png'))
+            # make a word cloud
+            if speaker in councilors:
+                wordcloud = WordCloud(max_font_size=40).generate(speaker_stats[speaker]["all_words"])
+                wordcloud.to_file(os.path.join(dir,speaker + '.wordcloud.png'))
 
 
     ncols = 4
     nrows = 4
     idx = 0
 
-	# find the subset of councilors present
+    # find the subset of councilors present
     present_councilors = []
     for speaker_id in speaker_stats.keys():
-    	if speaker_id in councilors: present_councilors.append(speaker_id)
+        if speaker_id in councilors: present_councilors.append(speaker_id)
 
     # make a table with stats
     for i in range(nrows):
-    	html.write('    <tr>\n')
-    	for j in range(ncols):
-		    html.write('      <td>\n')
-		    idx = i*ncols+j
-		    if idx < len(present_councilors):
-		    	if present_councilors[idx] in speaker_stats.keys():
-				    imagename = present_councilors[idx] + '.wordcloud.png'
-				    html.write('        <center>' + present_councilors[idx] + "</center><br>\n")
-				    html.write('        total time: ' + str(round(speaker_stats[present_councilors[idx]]["total_time"]/60.0,2)) + ' minutes<br>\n')
-				    html.write('        total words: ' + str(speaker_stats[present_councilors[idx]]["total_words"]) + '<br>\n')
-				    html.write('        <a href="' + imagename + '"><img src="' + imagename + '" height=150></img></a><br>\n')
-		    html.write('      </td>\n')
-    	html.write('    </tr>\n')
+        html.write('    <tr>\n')
+        for j in range(ncols):
+            html.write('      <td>\n')
+            idx = i*ncols+j
+            if idx < len(present_councilors):
+                if present_councilors[idx] in speaker_stats.keys():
+                    imagename = present_councilors[idx] + '.wordcloud.png'
+                    html.write('        <center>' + present_councilors[idx] + "</center><br>\n")
+                    html.write('        total time: ' + str(round(speaker_stats[present_councilors[idx]]["total_time"]/60.0,2)) + ' minutes<br>\n')
+                    html.write('        total words: ' + str(speaker_stats[present_councilors[idx]]["total_words"]) + '<br>\n')
+                    html.write('        <a href="' + imagename + '"><img src="' + imagename + '" height=150></img></a><br>\n')
+            html.write('      </td>\n')
+        html.write('    </tr>\n')
     html.write('  </table>\n')
 
     html.write('  </body>\n')
@@ -165,50 +184,73 @@ def srt2html(yt_id):
     html.close()
     eshtml.close()
 
+    video_data[yt_id]["last_update"] = time.time()
+    with open(jsonfile, "w") as fp:
+        json.dump(video_data, fp, indent=4)
+
 def make_index():
 
     import yt_dlp
+    # read in the speaker mappings
+    jsonfile = 'video_data.json'
+    if os.path.exists(jsonfile):
+        with open(jsonfile, 'r') as fp:
+            video_data = json.load(fp)
+    else:
+        video_data = {}
+
     htmlfiles = glob.glob('*/*.html')
     lines = []
     for htmlfile in htmlfiles:
-    	if htmlfile.endswith("es.html"): continue
-    	date = htmlfile.split('_')[0]
-    	yt_id = '_'.join(htmlfile.split('_')[1:]).split('\\')[0]
+        if htmlfile.endswith("es.html"): continue
+        date = htmlfile.split('_')[0]
+        yt_id = '_'.join(htmlfile.split('_')[1:]).split('\\')[0]
 
-    	eshtmlfile = os.path.splitext(htmlfile)[0]+'.es.html'
-    	url = "https://youtu.be/" + yt_id 
-    	info = yt_dlp.YoutubeDL().extract_info(url, download=False) 
-    	title = info["title"]
-    	channel = info["channel"]
-    	lines.append('<tr><td>' + date + '</td><td><a href="' + url + '">[Video]</a></td><td>' + channel + '</td><td>'+title+'</td><td><a href="' + htmlfile + '">English</a></td><td><a href="' + eshtmlfile + '">Spanish</a></td></tr>\n')
+        eshtmlfile = os.path.splitext(htmlfile)[0]+'.es.html'
+        url = "https://youtu.be/" + yt_id 
 
+        download = True
+        if yt_id in video_data.keys():
+            if "title" in video_data[yt_id].keys():
+                title = video_data[yt_id]["title"]
+                channel = video_data[yt_id]["channel"]
+                download = False
+
+        if download:
+            info = yt_dlp.YoutubeDL().extract_info(url, download=False) 
+            title = info["title"]
+            channel = info["channel"]
+            video_data[yt_id] = {}
+            video_data[yt_id]["title"] = title
+            video_data[yt_id]["channel"] = channel
+
+        lines.append('<tr><td>' + date + '</td><td><a href="' + url + '">[Video]</a></td><td>' + channel + '</td><td>'+title+'</td><td><a href="' + htmlfile + '">English</a></td><td><a href="' + eshtmlfile + '">Spanish</a></td></tr>\n')
+
+    with open(jsonfile, "w") as fp:
+        json.dump(video_data, fp, indent=4)
 
     lines.sort(reverse=True)
     index_page = open('index.html', 'w', encoding="utf-8")
     index_page.write("<table border=1>\n")
     index_page.write("<tr><td><center>Upload Date</center></td><td><center>YouTube</center></td><td><center>Channel</center></td><td><center>Title</center></td><td colspan=2><center>Transcript</center></td></tr>\n")
     for line in lines:
-    	index_page.write(line)
+        index_page.write(line)
     index_page.write("</table>")
     index_page.close()
 
 if __name__ == "__main__":
 
-	#make_index()
-	#ipdb.set_trace()
+    files = glob.glob("*/*.srt")
+    for file in files:
+        yt_id = '_'.join(file.split('_')[1:]).split('\\')[0]
+        dir = os.path.dirname(file)
+        srtfilename = os.path.join(dir,dir) + '.srt'
+        if srtfilename != file: continue
+        htmlfilename = os.path.join(dir,dir) + '.html'
 
-	files = glob.glob("*/*.srt")
-	for file in files:
-		yt_id = '_'.join(file.split('_')[1:]).split('\\')[0]
-		dir = os.path.dirname(file)
-		srtfilename = os.path.join(dir,dir) + '.srt'
-		htmlfilename = os.path.join(dir,dir) + '.html'
+        if os.path.exists(srtfilename): 
+            srt2html(yt_id)
 
-		# we want to regenerate them in case we update speaker_ids.json or the srt file
-		# it's pretty fast anyway
-		if os.path.exists(srtfilename): # and not os.path.exists(htmlfilename)
-			srt2html(yt_id)
-
-	# make the top level page with links to all transcripts
-	make_index()
+    # make the top level page with links to all transcripts
+    make_index()
 
